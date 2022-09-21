d9fe235ae104944f81226e04fff96a94603eb514
macasados <112190950+macasados@users.noreply.github.com>
2022-09-21 15:49:38 +0000

    Finished with step 15

--- .ccls-cache/@home@runner@WhileImportPractice/main.cpp
+++ .ccls-cache/@home@runner@WhileImportPractice/main.cpp
@@ -20,16 +20,35 @@ int main() {
   
   // open the input file
   inFile.open("TestResultsData.dat");
+
+  //open the output file
+  outFile.open(AnalyzedData.txt");
   
   //read in the date - first line of file 
   inFile >> date;
   cout << date << endl;
   
-  // read in the first 2 pieces of info on the second line
+// read in the first 2 pieces of info on the second line
   inFile >> firstName;
     cout << firstName << ' ';
   inFile >> TestResult;
     cout << TestResult << endl;
+  while(inFile)
+  {
+    //update number of cases & persons tested
+    cumulative_cases = cumulative_cases + TestResult;
+    count++; // increment the number of patients
+
+    //read in the next line
+    inFile >> firstName;
+    inFile >> TestResult;
+  }
+  
+  cout << "Total Number of Cases = " << cumulative_cases << endl;
+  cout << "Number of persons tested = " << count << endl;
+  
+  inFile.close();
+  
   
 
 
