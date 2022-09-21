14d192d3184a4f8f23d33b73c2d12933777388fe
macasados <112190950+macasados@users.noreply.github.com>
2022-09-21 15:31:18 +0000

    First two lines of file

--- .ccls-cache/@home@runner@WhileImportPractice/main.cpp
+++ .ccls-cache/@home@runner@WhileImportPractice/main.cpp
@@ -3,10 +3,33 @@
 #include <string>
 #include <iomanip>
 
-#include namespace std;
+using namespace std;
 
 
 int main() {
+  //declare variables
+  string firstName;
+  string date;
+  int TestResult;
+  int count = 0;
+  int cumulative_cases = 0;
+  
+  //declare stream variables
+  ifstream inFile; // needed to access the file we want to load
+  ofstream outFile; // needed to save teh results of the analysis
+  
+  // open the input file
+  inFile.open("TestResultsData.dat");
+  
+  //read in the date - first line of file 
+  inFile >> date;
+  cout << date << endl;
+  
+  // read in the first 2 pieces of info on the second line
+  inFile >> firstName;
+    cout << firstName << ' ';
+  inFile >> TestResult;
+    cout << TestResult << endl;
   
 
 
